5df0febfc40bb92d01f197177d9e7188eae35472
L SURYA <94703794+Suryal3001@users.noreply.github.com>
2021-11-27 10:32:32 +0530

    Add files via upload

---
+++ mycap prj 4.py
@@ -0,0 +1,13 @@
+test_str = str(input("Enetr any word:-"))
+  
+
+all_freq = {}
+  
+for i in test_str:
+    if i in all_freq:
+        all_freq[i] += 1
+    else:
+        all_freq[i] = 1
+  
+
+print ( str(all_freq))
